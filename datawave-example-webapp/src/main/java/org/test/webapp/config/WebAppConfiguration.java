c6f80f3d45c469c2e47d83d75992a6dde18c88ea
Alan Hay <alanhay99@hotmail.com>
2018-02-26 22:52:53 +0000

    Added handler for index.html

--- datawave-example-webapp/src/main/java/org/test/webapp/config/WebAppConfiguration.java
+++ datawave-example-webapp/src/main/java/org/test/webapp/config/WebAppConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Import({ RepositoryRestMvcConfiguration.class })
 @EnableWebMvc
@@ -18,6 +19,11 @@ public class WebAppConfiguration extends RepositoryRestMvcConfiguration {
 
 	public WebAppConfiguration(ApplicationContext context, ObjectFactory<ConversionService> conversionService) {
 		super(context, conversionService);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/index.html").addResourceLocations("/index.html");
 	}
 
 	@Bean
